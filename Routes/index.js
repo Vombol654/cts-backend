3db23eed5d7ccb1141a265314871dd01171a6730
Vombol654 <97107431+Vombol654@users.noreply.github.com>
2022-06-26 18:01:12 +0530

    Add files via upload

--- Routes/index.js
+++ Routes/index.js
@@ -3,8 +3,7 @@ const languagesController=require('../Controllers/languages')
 const coursetypesController=require('../Controllers/coursetypes')
 const mentordetailsController=require('../Controllers/mentordetails')
 const userController=require('../Controllers/users')
-// const paymentController = require('../Controllers/payments');
-const paymentController = require('../Controllers/payment');
+const paymentController = require('../Controllers/payments');
 const route=express.Router()
 route.get('/language',languagesController.getLanguages)
 route.get('/coursetypes',coursetypesController.getcoursetypes)
@@ -13,7 +12,7 @@ route.post('/signup',userController.getusers)
 route.post('/login',userController.loginusers)
 route.post('/filter',mentordetailsController.filteredmentordetails)
 route.get('/mentordetail/:mentorId',mentordetailsController.getmentordetailsByMentorId)
-route.get('/createorder',paymentController.payments)
-// route.post('/payment/callback',paymentController.paymentCallback)
-route.get('/payments/:paymentId',paymentController.getPayment)
+route.post('/payment', paymentController.payment);
+route.post('/callback', paymentController.callback);
+
 module.exports=route
